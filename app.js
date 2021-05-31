d28c865816bf0f5d9b415ef97c7eb699f4633ccd
z00466ch <yamini.matta@siemens-healthineers.com>
2021-05-31 20:14:14 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,48 +1,29 @@
 
-function  addList(){
+function addList() {
 
-var todo = document.getElementById("addList").value;
+    var todo = document.getElementById("addList").value;
 
-var li= document.createElement("li");
+    var li = document.createElement("li");
 
- var textli= document.createTextNode(todo);
+    var textli = document.createTextNode(todo);
 
- li.appendChild(textli);
+    li.appendChild(textli);
 
- document.getElementById("list").appendChild(li);
+    document.getElementById("list").appendChild(li);
 
-var Mylist = document.getElementById("list");
+    var Mylist = document.getElementById("list");
 
-//Adding checkbox
+    //Adding delete button to element
 
-// let check = document.createElement("input");
-// check.type ="checkbox";
-// check.name ="checkbox"; check.id="check";
-// check.setAttribute('id',"checkbox-id");
-// Mylist.appendChild(check);
+    let deleteButton = document.createElement("button");
 
-// console.log(document.getElementById("checkbox-id").value);
+    deleteButton.appendChild(document.createTextNode("delete"));
 
+    Mylist.appendChild(deleteButton);
 
-
-//Adding delete button to element
-
-let deleteButton = document.createElement("button");
-
- deleteButton.appendChild(document.createTextNode("delete"));
-
- Mylist.appendChild(deleteButton);
- 
-
- deleteButton.addEventListener("click",function(e){
-      li.parentNode.removeChild(deleteButton);
-      li.parentNode.removeChild(li);
-
-  
-      
- });
-
-//Edit button in todo List
-
+    deleteButton.addEventListener("click", function (e) {
+        li.parentNode.removeChild(deleteButton);
+        li.parentNode.removeChild(li);
+    });
 
 }
